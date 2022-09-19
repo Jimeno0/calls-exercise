78b9b6a119322b8ae70814fdd4931bcf7a8247ab
jimeno0 <jimenocontact@gmail.com>
2022-09-19 19:38:13 +0200

    add tracktor lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Tractor, Spacer, Typography } from "@aircall/tractor";
 
 function App() {
   return (
     <div className="App">
+      <Tractor injectStyle>
+        <Spacer space="s">
+          <Typography variant="displayM">Hello</Typography>
+          <Typography variant="displayL">World</Typography>
+        </Spacer>
+      </Tractor>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
