3aedaae7cc229b9111947337af610faf4d518cbe
jimeno0 <jimenocontact@gmail.com>
2022-09-19 19:43:01 +0200

    cleanup scaffilding

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { Tractor, Spacer, Typography } from "@aircall/tractor";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Tractor injectStyle>
-        <Spacer space="s">
-          <Typography variant="displayM">Hello</Typography>
-          <Typography variant="displayL">World</Typography>
-        </Spacer>
-      </Tractor>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Tractor injectStyle>
+      <Spacer space="s">
+        <Typography variant="displayM">Hello</Typography>
+        <Typography variant="displayL">World</Typography>
+      </Spacer>
+    </Tractor>
   );
-}
+};
 
 export default App;
