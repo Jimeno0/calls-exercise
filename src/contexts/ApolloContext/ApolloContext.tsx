f89f0e2bdb8c45b8da401fd4a8fbdb9db2e95450
jimeno0 <jimenocontact@gmail.com>
2022-09-25 12:19:13 +0200

    dont set token on login req

--- src/contexts/ApolloContext/ApolloContext.tsx
+++ src/contexts/ApolloContext/ApolloContext.tsx
@@ -23,11 +23,13 @@ const httpLink = new HttpLink({ uri: API_URL });
 
 const wsLink = new WebSocketLink(
   new SubscriptionClient(API_SOCKET, {
-    connectionParams: {
+    reconnect: true,
+    connectionParams: () => ({
       authorization: `Bearer ${localStorageManager.get("access_token")}`,
-    },
+    }),
   })
 );
+console.log({ wsLink });
 
 const splitLink = split(
   ({ query }) => {
@@ -45,10 +47,12 @@ const authMiddleware = new ApolloLink((operation, forward) => {
   operation.setContext(({ headers = {} }) => {
     const operationName = operation.operationName;
     const isRefreshMutation = operationName === "RefreshToken";
+    const isLoginMutation = operationName === "Login";
+
     const token = isRefreshMutation
       ? localStorageManager.get("refresh_token")
       : localStorageManager.get("access_token");
-    const bearer = token ? `Bearer ${token}` : "";
+    const bearer = !isLoginMutation && token ? `Bearer ${token}` : "";
     return {
       headers: {
         ...headers,
